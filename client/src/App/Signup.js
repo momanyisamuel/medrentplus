9f7af162a526c8278d79e29c9bd5e37d7740f247
momanyisamuel <momanyisamuel48@gmail.com>
2021-02-15 21:54:09 +0300

    adding signup

--- client/src/App/Signup.js
+++ client/src/App/Signup.js
@@ -1,11 +1,44 @@
 import React from "react";
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  constOnFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div class="pa4 black-80 center mt5">
-      <form class="measure center">
+      <form class="measure center" onSubmit={onFormSubmit}>
         <div id="sign_up" class="ba b--transparent ph0 mh0">
           <h3 class="f4 fw6 ph0 mh0">Sign Up</h3>
+          <div class="mt3">
+            <label class="db fw6 lh-copy f6" for="firstName">
+              First Name
+            </label>
+            <input
+              class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+              type="text"
+              name="firstName"
+              id="firstName"
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </div>
+          <div class="mt3">
+            <label class="db fw6 lh-copy f6" for="lastName">
+              Last Name
+            </label>
+            <input
+              class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+              type="text"
+              name="lastName"
+              id="lastName"
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </div>
           <div class="mt3">
             <label class="db fw6 lh-copy f6" for="email-address">
               Email
@@ -15,6 +48,7 @@ const Signup = () => {
               type="email"
               name="email-address"
               id="email-address"
+              onChange={(event) => setEmail(event.target.value)}
             />
           </div>
           <div class="mv3">
@@ -26,6 +60,7 @@ const Signup = () => {
               type="password"
               name="password"
               id="password"
+              onChange={(event) => setPassword(event.target.value)}
             />
           </div>
         </div>
@@ -38,7 +73,7 @@ const Signup = () => {
         </div>
         <div class="lh-copy mt3">
           <a href="#0" class="f6 link dim black db">
-            Sign in
+            Sign Up
           </a>
         </div>
       </form>
